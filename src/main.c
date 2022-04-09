843883187150de2071c235bd7e51b2998e434cf1
Killaship <69988679+Killaship@users.noreply.github.com>
2022-04-08 21:33:19 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-int *blocks[64];
+int *iblocks[32];
+int *dblocks[64];
+struct sblock {
+	int inodebitmap[32] = {0}; // bitmap of free inodes
+	int dblckbitmap[64] = {0}; // bitmap of free data blocks
+	int inodes;
+	int dblocks;
+	int itblstart;
+	int dtblstart;
+}
+struct inode {
+	char *filename[32]; // filename, including null char
+	int perms; // permissions, will figure out later
+	int owner; // uid of owner, figure out later
+	int size; // size of file in blocks
+	int blockstart; // block file begins at
+	int timestamp; // unix timestamp of when file was created
+	int modstamp; // unix timestamp of when file was modified
+}
 int main() {
 	for(int i = 0; i < 63; i++) {
-		blocks[i] = malloc(4096);
+		dblocks[i] = malloc(4096);
 	}
+	
 	return 0;
 }
